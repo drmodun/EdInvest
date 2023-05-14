aa46f19122327f77a9308ebb7cdc2b7cf383c0bc
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-05-14 20:34:12 +0200

    updated header color variability

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,6 @@
 const HomePage = () => {
+  document.querySelector(':root').style.setProperty('--header-background', 'var(--color-blue)');
+  document.querySelector(':root').style.setProperty('--header-nav-color', 'white');
   return <div className="headerMargin">Home page</div>;
 };
 
