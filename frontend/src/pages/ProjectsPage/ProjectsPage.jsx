560f73a5d3587a3d31365a5b2594e50c9ca2b335
lovretomic <tomiclovre05@gmail.com>
2023-05-14 21:54:07 +0200

    added projects title

--- frontend/src/pages/ProjectsPage/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,5 +1,14 @@
+import classes from "./index.module.css";
+
 const ProjectsPage = () => {
-  return <>Projects page</>;
+  return (
+    <>
+      <section className={classes.sectionSearch}>
+        <h1 className={classes.sectionSearchTitle}>Find your project</h1>
+        <div className={classes.sectionSearchInput}></div>
+      </section>
+    </>
+  );
 };
 
 export default ProjectsPage;
