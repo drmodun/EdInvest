fb6b789a7bbc581dd0427a465a3e9ea51aa621b6
lovretomic <tomiclovre05@gmail.com>
2023-05-14 22:50:49 +0200

    updated take a look section

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,7 +1,13 @@
 import classes from "./index.module.css";
 
+import PlaceholderImage from "../../assets/images/placeholder.jpg";
+
 const Card = () => {
-  return <div className={classes.card}></div>;
+  return (
+    <div className={classes.card}>
+      <img src={PlaceholderImage} alt="Placeholder" />
+    </div>
+  );
 };
 
 export default Card;
