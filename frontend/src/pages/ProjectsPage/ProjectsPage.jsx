185a3c6235a75a6fadeebf745200574bffaf68c0
lovretomic <tomiclovre05@gmail.com>
2023-05-14 22:05:19 +0200

    added search bar

--- frontend/src/pages/ProjectsPage/ProjectsPage.jsx
+++ frontend/src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,11 +1,19 @@
 import classes from "./index.module.css";
 
+import SearchIcon from "../../assets/icons/search.svg";
+
 const ProjectsPage = () => {
   return (
     <>
       <section className={classes.sectionSearch}>
         <h1 className={classes.sectionSearchTitle}>Find your project</h1>
-        <div className={classes.sectionSearchInput}></div>
+        <div className={classes.sectionSearchQuery}>
+          <img src={SearchIcon} />
+          <input
+            type="text"
+            placeholder="Search for a project, organisation or something else"
+          />
+        </div>
       </section>
     </>
   );
