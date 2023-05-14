f2bf6ea2b1e46b1dcfca2c0fd185a0373aa69a5c
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-05-14 20:08:56 +0200

    implemented layout spacing

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,28 @@
 import classes from "./index.module.css";
 
 import Logo from "../../assets/logo.svg";
-import Button from "../Button"
+import Button from "../Button";
 
 const Navigation = () => {
-  return <header className={classes.header}>
-    <img src={Logo} alt="Logo" className={classes.headerLogo} />
-    <nav className={classes.headerNav}>
-      <a href="" className={classes.headerNavItem}>Home</a>
-      <a href="" className={classes.headerNavItem}>About us</a>
-      <a href="" className={classes.headerNavItem}>Projects</a>
-    </nav>
-    <Button className={classes.headerButton}>Sign in</Button>
-  </header>;
+  return (
+    <header className={classes.header}>
+      <div className="layoutSpacing">
+        <img src={Logo} alt="Logo" className={classes.headerLogo} />
+        <nav className={classes.headerNav}>
+          <a href="" className={classes.headerNavItem}>
+            Home
+          </a>
+          <a href="" className={classes.headerNavItem}>
+            About us
+          </a>
+          <a href="" className={classes.headerNavItem}>
+            Projects
+          </a>
+        </nav>
+        <Button className={classes.headerButton}>Sign in</Button>
+      </div>
+    </header>
+  );
 };
 
 export default Navigation;
