37849c7ba2d397c537773b0b13e7a51e40640922
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-05-14 19:01:38 +0200

    added fonts

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -5,6 +5,11 @@ import Logo from "../../assets/logo.svg";
 const Navigation = () => {
   return <header className={classes.header}>
     <img src={Logo} alt="Logo" className={classes.headerLogo} />
+    <nav className={classes.headerNav}>
+      <a href="" className={classes.headerNavItem}>Home</a>
+      <a href="" className={classes.headerNavItem}>About us</a>
+      <a href="" className={classes.headerNavItem}>Projects</a>
+    </nav>
   </header>;
 };
 
