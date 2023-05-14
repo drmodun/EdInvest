6a25d82fd50d803cf39040d103438c6ba1f21751
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2023-05-14 18:44:49 +0200

    updated header

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,5 +1,11 @@
+import classes from "./index.module.css";
+
+import Logo from "../../assets/logo.svg";
+
 const Navigation = () => {
-  return <header>Navigation</header>;
+  return <header className={classes.header}>
+    <img src={Logo} alt="Logo" className={classes.headerLogo} />
+  </header>;
 };
 
 export default Navigation;
