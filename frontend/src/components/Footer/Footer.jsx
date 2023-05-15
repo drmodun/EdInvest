6633846b4afabb701f55ec640b49de11c2e67922
lovretomic <tomiclovre05@gmail.com>
2023-05-15 18:55:05 +0200

    added links and copyright statement

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,5 +1,28 @@
+import classes from "./index.module.css";
+
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className={classes.footer}>
+      <div className="layoutSpacing">
+        <div className={classes.footerLinks}>
+          <nav className={classes.footerLinksNav}>
+            <a href="/">Home</a>
+            <a href="">About us</a>
+            <a href="/projects">Projects</a>
+          </nav>
+          <div className={classes.footerLinksIcons}>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+        <p className={classes.footerCopy}>
+          © 2023 EdInvest. Sva prava pridržana
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
