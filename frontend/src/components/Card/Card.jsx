d0ad934665fa7e799e385cf1e4a4871ccb9fdefc
lovretomic <tomiclovre05@gmail.com>
2023-05-15 15:53:18 +0200

    added card action buttons

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -3,17 +3,32 @@ import classes from "./index.module.css";
 import PlaceholderImage from "../../assets/images/placeholder.jpg";
 import BlueArrow from "../../assets/icons/blue-arrow.svg";
 import WhiteArrow from "../../assets/icons/white-arrow.svg";
+import VerifiedCheckmark from "../../assets/icons/verified-checkmark.svg";
+
+import Heart from "../../assets/icons/heart.svg";
+import Share from "../../assets/icons/share.svg";
 
 const Card = () => {
   return (
     <div className={classes.card}>
+      <div className={classes.cardActions}>
+        <button className={classes.cardActionsButton}>
+          <img src={Heart} alt="" />
+        </button>
+        <button className={classes.cardActionsButton}>
+          <img src={Share} alt="" />
+        </button>
+      </div>
       <img
         src={PlaceholderImage}
         alt="Placeholder"
         className={classes.cardImage}
       />
       <div className={classes.cardContent}>
-        <h3 className={classes.companyTitle}>Company Title</h3>
+        <h3 className={classes.companyTitle}>
+          Company Title
+          <img src={VerifiedCheckmark} alt="" />
+        </h3>
         <p className={classes.companyType}>Company Type</p>
         <p className={classes.companyDescription}>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
@@ -26,7 +41,7 @@ const Card = () => {
         </a>
         <div className={classes.companyRaised}>
           <p className={classes.companyRaisedValue}>
-            $479 <span>Raised</span>
+            $999 <span>Raised</span>
           </p>
           <button className={classes.companyInvest}>
             Invest
