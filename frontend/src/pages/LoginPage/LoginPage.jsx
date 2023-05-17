42f71018eaeedc9813258d35e1c3050567cb0ec9
lovretomic <tomiclovre05@gmail.com>
2023-05-17 19:23:35 +0200

    added sign in close button

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import classes from "./LoginPage.module.css";
 import { login } from "../../axios/UserCalls/UserApiCalls";
+
+import classes from "./index.module.css";
+
 import Facebook from "../../assets/facebook.png";
 import Google from "../../assets/google.png";
 import Twitter from "../../assets/twitter.png";
@@ -10,13 +12,16 @@ import Torus from "../../assets/torus.png";
 import Metamask from "../../assets/metamask.png";
 import BackgroundImage from "../../assets/login-background.png";
 import MobileBackgroundImage from "../../assets/header-background.png";
+import CloseIcon from "../../assets/icons/close.svg";
+
 import useUserInfo from "../../Providers/UserInfoProvider";
+
 export const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
   const [windowSize, setWindowSize] = useState(getWindowSize());
-    const userInfo = useUserInfo();
+  const userInfo = useUserInfo();
 
   function getWindowSize() {
     const { innerWidth, innerHeight } = window;
@@ -48,14 +53,20 @@ export const LoginPage = () => {
       setError("Invalid credentials");
     }
   };
-  function logOutAndRefresh(){
+  function logOutAndRefresh() {
     userInfo.logOut();
     window.location.replace("/login");
-    }
+  }
 
   console.log(windowSize);
   return (
     <div className={classes.LoginPage}>
+      <button
+        className={classes.CloseButton}
+        onClick={() => (window.location.href = "/")}
+      >
+        <img src={CloseIcon} alt="" />
+      </button>
       <img
         className={classes.MainImage}
         src={
@@ -64,57 +75,59 @@ export const LoginPage = () => {
         alt="logo"
       />
       {!userInfo.isLogged ? (
-      <div className={classes.Logins}>
-        <span className={classes.Title}>Sign in</span>
-        <span className={classes.SubTitle}>
-          Please Sign into your account to start using EdInvest
-        </span>
-        <form className={classes.LoginForm} onSubmit={handleSubmit}>
-          <input
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <input
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button type="submit">Login</button>
-          <p className={classes.Error}>{error ? error : ""}</p>
-        </form>
-        <div className={classes.Border}> or </div>
-        <div className={classes.AlternateLogins}>
-          <button className={classes.CryptoLogin}>
-            <img src={Torus} alt="Torus" />
-            <span> Sign in with Torus</span>
-          </button>
-          <button className={classes.CryptoLogin}>
-            <img src={Metamask} alt="Metamask" />
-            <span> Sign in with Metamask</span>
-          </button>
+        <div className={classes.Logins}>
+          <span className={classes.Title}>Sign in</span>
+          <span className={classes.SubTitle}>
+            Please Sign into your account to start using EdInvest
+          </span>
+          <form className={classes.LoginForm} onSubmit={handleSubmit}>
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button type="submit">Login</button>
+            <p className={classes.Error}>{error ? error : ""}</p>
+          </form>
           <div className={classes.Border}> or </div>
-          <div className={classes.SocialLogins}>
-            <img src={Facebook} alt="FaceBook" />
-            <img src={Google} alt="Google" />
-            <img src={Twitter} alt="Twitter" />
-            <img src={AppStore} alt="AppStore" />
-          </div>
-          <div className={classes.SignUp}>
-            <span>Don't have an account? </span>
-            <Link to={"/register"}>
-              <span>Sign Up</span>
-            </Link>
+          <div className={classes.AlternateLogins}>
+            <button className={classes.CryptoLogin}>
+              <img src={Torus} alt="Torus" />
+              <span> Sign in with Torus</span>
+            </button>
+            <button className={classes.CryptoLogin}>
+              <img src={Metamask} alt="Metamask" />
+              <span> Sign in with Metamask</span>
+            </button>
+            <div className={classes.Border}> or </div>
+            <div className={classes.SocialLogins}>
+              <img src={Facebook} alt="FaceBook" />
+              <img src={Google} alt="Google" />
+              <img src={Twitter} alt="Twitter" />
+              <img src={AppStore} alt="AppStore" />
+            </div>
+            <div className={classes.SignUp}>
+              <span>Don't have an account? </span>
+              <Link to={"/register"}>
+                <span>Sign Up</span>
+              </Link>
+            </div>
           </div>
         </div>
-      </div>
       ) : (
         <div className={classes.AlreadyLogged}>
-            <span>You are already logged in</span>
-            <Link to={"/"}> Go to Home Page</Link>
-            <button className={classes.LogOut} onClick={logOutAndRefresh}>Log out</button>
+          <span>You are already logged in</span>
+          <Link to={"/"}> Go to Home Page</Link>
+          <button className={classes.LogOut} onClick={logOutAndRefresh}>
+            Log out
+          </button>
         </div>
       )}
     </div>
