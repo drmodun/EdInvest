f28ec128a818f9ab4339c4ef9c3a9946df908d9d
tsiklic1 <tsiklic@pmfst.hr>
2023-05-18 16:38:44 +0200

    adding donation info to page

--- frontend/src/components/ProjectPageComponents/EventDonationInfo/EventDonationInfo.jsx
+++ frontend/src/components/ProjectPageComponents/EventDonationInfo/EventDonationInfo.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getInvestorsByItemId } from "../../../axios/InvestmentsApiCalls";
+import classes from "../index.module.css";
 
 const EventDonationInfo = ({ project }) => {
   const [investments, setInvestments] = useState([]);
@@ -17,16 +18,30 @@ const EventDonationInfo = ({ project }) => {
 
   return (
     <>
-      <div>
-        {investments.map((investment, i) => (
-          <div key={i}>{investment.name}</div>
-        ))}
+      <div className={classes.TotalDonationsWrapper}>
+        <h2>
+          {investments.reduce((sum, project) => sum + project.amount, 0)}€
+        </h2>
+        <p>
+          {project.name} has raised this amount so far. Check down below to see
+          all donations
+        </p>
       </div>
 
-      <div>
-        {investments.map((investment, i) => (
-          <div key={i}>{investment.amount}</div>
-        ))}
+      <div className={classes.DonationInfoWrapper}>
+        <div>
+          <h4>Contributor</h4>
+          {investments.map((investment, i) => (
+            <div key={i}>{investment.name}</div>
+          ))}
+        </div>
+
+        <div>
+          <h4>Value in euors</h4>
+          {investments.map((investment, i) => (
+            <div key={i}>€{investment.amount}</div>
+          ))}
+        </div>
       </div>
     </>
   );
