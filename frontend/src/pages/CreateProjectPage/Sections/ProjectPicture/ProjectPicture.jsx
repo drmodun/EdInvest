576f3f5db665f01bab746c032d67cd3976ed601a
lovretomic <tomiclovre05@gmail.com>
2023-05-18 17:23:18 +0200

    added drag and drop functionality

--- frontend/src/pages/CreateProjectPage/Sections/ProjectPicture/ProjectPicture.jsx
+++ frontend/src/pages/CreateProjectPage/Sections/ProjectPicture/ProjectPicture.jsx
@@ -1,9 +1,33 @@
+import { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+
 import classes from "../../index.module.css";
+import DownloadIcon from "../../../../assets/icons/download.svg";
+
+// REQUIRES react-dropzone PACKAGE
 
 const ProjectPicture = () => {
+  const onDrop = useCallback((acceptedFiles) => {
+    // Do something with the files
+  }, []);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
   return (
     <>
-      <h1>Project Picture</h1>
+      <section className={classes.section}>
+        <h3 className={classes.sectionTitle}>Profile picture</h3>
+        <p className={classes.sectionDescription}>
+          This will be your header picture of your project. You can change this
+          later in settings.
+        </p>
+      </section>
+      <div className={classes.sectionDropzone} {...getRootProps()}>
+        <input className={classes.sectionDropzoneInput} {...getInputProps()} />
+        <img src={DownloadIcon} alt="" />
+        <h4>Drag & drop files or Browse</h4>
+        <p>Supported formates: JPEG, PNG, GIF, MP4, PDF, PSD, AI, Word, PPT</p>
+      </div>
     </>
   );
 };
