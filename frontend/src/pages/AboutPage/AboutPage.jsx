33eee94990b2985d79678a45185f30877b1ce71a
tsiklic1 <tsiklic@pmfst.hr>
2023-05-19 19:27:51 +0200

    adding more info section

--- frontend/src/pages/AboutPage/AboutPage.jsx
+++ frontend/src/pages/AboutPage/AboutPage.jsx
@@ -6,6 +6,20 @@ const AboutPage = () => {
       <div className={`headerMargin ` + classes.HeaderWrapper}>
         <h2 className={classes.HeaderTitle}>EdInvest</h2>
       </div>
+      <div className={classes.MoreInfoSection}>
+        <div className={classes.MoreInfoSectionLeft}>
+          <h3 className={classes.MoreInfoTitle}>What is EdInvest?</h3>
+          <p className={classes.MoreInfoText}>
+            EdInvest is an innovative platform that uses crypto funding to
+            support education projects. Our platform allows individuals to
+            contribute, invest, and donate to a wide range of education
+            projects. By accepting cryptocurrencies, we make it easy for people
+            around the world to get involved.
+          </p>
+        </div>
+        <div className={classes.MoreInfoImg}></div>
+        <div className={classes.MoreInfoSectionRight}></div>
+      </div>
     </>
   );
 };
