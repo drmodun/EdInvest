a2c7345a38f5b9a4394c8c765def63d1d8dcea3f
lovretomic <tomiclovre05@gmail.com>
2023-05-19 16:17:49 +0200

    added final message section

--- frontend/src/pages/CreateProjectPage/CreateProjectPage.jsx
+++ frontend/src/pages/CreateProjectPage/CreateProjectPage.jsx
@@ -6,6 +6,7 @@ import classes from "./index.module.css";
 import GeneralInformations from "./Sections/GeneralInformations";
 import ProjectPicture from "./Sections/ProjectPicture";
 import ReceivingFunds from "./Sections/ReceivingFunds";
+import FinalMessage from "./Sections/FinalMessage";
 
 const CreateProjectPage = () => {
   const [section, setSection] = useState(1);
@@ -21,43 +22,50 @@ const CreateProjectPage = () => {
 
   return (
     <div className={"layoutSpacing " + classes.content}>
-      <h1 className={classes.title}>Create a project</h1>
-      <div className={classes.sectionNav}>
-        <button
-          className={clsx({
-            [classes.sectionNavButton]: true,
-            [classes.active]: section === 1,
-          })}
-          onClick={() => handleSectionChange(1)}
-        >
-          General informations
-        </button>
-        <button
-          className={clsx({
-            [classes.sectionNavButton]: true,
-            [classes.active]: section === 2,
-          })}
-          onClick={() => handleSectionChange(2)}
-        >
-          Project picture
-        </button>
-        <button
-          className={clsx({
-            [classes.sectionNavButton]: true,
-            [classes.active]: section === 3,
-          })}
-          onClick={() => handleSectionChange(3)}
-        >
-          Receiving funds
-        </button>
-      </div>
+      {section < 4 ? (
+        <>
+          <h1 className={classes.title}>Create a project</h1>
+          <div className={classes.sectionNav}>
+            <button
+              className={clsx({
+                [classes.sectionNavButton]: true,
+                [classes.active]: section === 1,
+              })}
+              onClick={() => handleSectionChange(1)}
+            >
+              General informations
+            </button>
+            <button
+              className={clsx({
+                [classes.sectionNavButton]: true,
+                [classes.active]: section === 2,
+              })}
+              onClick={() => handleSectionChange(2)}
+            >
+              Project picture
+            </button>
+            <button
+              className={clsx({
+                [classes.sectionNavButton]: true,
+                [classes.active]: section === 3,
+              })}
+              onClick={() => handleSectionChange(3)}
+            >
+              Receiving funds
+            </button>
+          </div>
+        </>
+      ) : null}
       {section === 1 ? <GeneralInformations /> : null}
       {section === 2 ? <ProjectPicture /> : null}
       {section === 3 ? <ReceivingFunds /> : null}
+      {section === 4 ? <FinalMessage /> : null}
 
-      <button className={classes.continueButton} onClick={increaseSection}>
-        {section === 3 ? "Publish" : "Continue"}
-      </button>
+      {section < 4 ? (
+        <button className={classes.continueButton} onClick={increaseSection}>
+          {section === 3 ? "Publish" : "Continue"}
+        </button>
+      ) : null}
     </div>
   );
 };
---
+++ frontend/src/pages/CreateProjectPage/Sections/FinalMessage/FinalMessage.jsx
@@ -0,0 +1,5 @@
+const FinalMessage = () => {
+  return <h1>Wooohoo!</h1>;
+};
+
+export default FinalMessage;
